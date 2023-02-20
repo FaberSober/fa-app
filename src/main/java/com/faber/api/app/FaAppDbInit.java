d8fbcca7d0a274167c4d5afd1ab1d665dbe783f9
xupengfei <faberxu@gmail.com>
2023-02-20 14:17:57 +0800

    feat: base-数据库初始化

---
+++ src/main/java/com/faber/api/app/FaAppDbInit.java
@@ -0,0 +1,40 @@
+package com.faber.api.app;
+
+import com.faber.core.config.dbinit.DbInit;
+import com.faber.core.config.dbinit.vo.FaDdl;
+import com.faber.core.config.dbinit.vo.FaDdlSql;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Farando
+ * @date 2023/2/18 20:12
+ * @description
+ */
+@Slf4j
+@Service
+public class FaAppDbInit implements DbInit {
+
+    @Override
+    public String getNo() {
+        return "fa-app";
+    }
+
+    @Override
+    public String getName() {
+        return "APP模块";
+    }
+
+    @Override
+    public List<FaDdl> getDdlList() {
+        List<FaDdl> list = new ArrayList<>();
+
+        list.add(new FaDdl(100_000_000L, "V1.0.0", "初始化")
+                .addSql(new FaDdlSql("初始化", "sql/1.0.0_app_full.sql")));
+
+        return list;
+    }
+}
