27b61f1a820a1f211663275133ce0516934e1445
xupengfei <faberxu@gmail.com>
2023-02-23 14:37:53 +0800

    feat: base-菜单ID整理

--- src/main/resources/sql/1.0.0_app_data.sql
+++ src/main/resources/sql/1.0.0_app_data.sql
@@ -5,10 +5,10 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Records of base_rbac_menu
 -- ----------------------------
 BEGIN;
-INSERT INTO `base_rbac_menu` (`id`, `parent_id`, `name`, `sort`, `level`, `icon`, `status`, `link_type`, `link_url`, `crt_time`, `crt_user`, `crt_name`, `crt_host`, `upd_time`, `upd_user`, `upd_name`, `upd_host`, `deleted`) VALUES (200000, 0, 'APP管理', 3, 0, 'mobile-retro', 1, 1, '/admin/app', '2023-01-20 14:22:45', '1', '超级管理员', '127.0.0.1', NULL, NULL, NULL, NULL, 0);
+INSERT INTO `base_rbac_menu` (`id`, `parent_id`, `name`, `sort`, `level`, `icon`, `status`, `link_type`, `link_url`, `crt_time`, `crt_user`, `crt_name`, `crt_host`, `upd_time`, `upd_user`, `upd_name`, `upd_host`, `deleted`) VALUES (21000000, 0, 'APP管理', 3, 0, 'mobile-retro', 1, 1, '/admin/app', '2023-01-20 14:22:45', '1', '超级管理员', '127.0.0.1', NULL, NULL, NULL, NULL, 0);
 
-INSERT INTO `base_rbac_menu` (`id`, `parent_id`, `name`, `sort`, `level`, `icon`, `status`, `link_type`, `link_url`, `crt_time`, `crt_user`, `crt_name`, `crt_host`, `upd_time`, `upd_user`, `upd_name`, `upd_host`, `deleted`) VALUES (210000, 200000, 'APP版本管理', 0, 1, 'store', 1, 1, '/admin/app/app/apk', '2023-01-20 14:23:14', '1', '超级管理员', '127.0.0.1', NULL, NULL, NULL, NULL, 0);
-INSERT INTO `base_rbac_menu` (`id`, `parent_id`, `name`, `sort`, `level`, `icon`, `status`, `link_type`, `link_url`, `crt_time`, `crt_user`, `crt_name`, `crt_host`, `upd_time`, `upd_user`, `upd_name`, `upd_host`, `deleted`) VALUES (220000, 200000, 'APP崩溃日志', 1, 1, 'burst', 1, 1, '/admin/app/crash/apkCrash', '2023-01-20 14:23:29', '1', '超级管理员', '127.0.0.1', NULL, NULL, NULL, NULL, 0);
+INSERT INTO `base_rbac_menu` (`id`, `parent_id`, `name`, `sort`, `level`, `icon`, `status`, `link_type`, `link_url`, `crt_time`, `crt_user`, `crt_name`, `crt_host`, `upd_time`, `upd_user`, `upd_name`, `upd_host`, `deleted`) VALUES (21010000, 21000000, 'APP版本管理', 0, 1, 'store', 1, 1, '/admin/app/app/apk', '2023-01-20 14:23:14', '1', '超级管理员', '127.0.0.1', NULL, NULL, NULL, NULL, 0);
+INSERT INTO `base_rbac_menu` (`id`, `parent_id`, `name`, `sort`, `level`, `icon`, `status`, `link_type`, `link_url`, `crt_time`, `crt_user`, `crt_name`, `crt_host`, `upd_time`, `upd_user`, `upd_name`, `upd_host`, `deleted`) VALUES (21020000, 21000000, 'APP崩溃日志', 1, 1, 'burst', 1, 1, '/admin/app/crash/apkCrash', '2023-01-20 14:23:29', '1', '超级管理员', '127.0.0.1', NULL, NULL, NULL, NULL, 0);
 COMMIT;
 
 
